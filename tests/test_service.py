4c21408cb96b9cd2e0c3430b477d52f0a2cfc012
Vishal Nayek <nayek@Vishals-MacBook-Air.local>
2026-09-23 01:11:09 +0530

    Added test

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,16 +1,20 @@
-from service import RepositoryService
+from service import RepositoryService, RepositoryNotFoundError
 from unittest.mock import Mock
+import requests
+import pytest
 
-github_client = Mock()
 
-github_client.get_repo.return_value = {
-    "name": "task-manager",
-    "language": "Python"
-}
-
-service = RepositoryService(github_client)
 
 def test_get_repository_success():
+
+    github_client = Mock()
+
+    github_client.get_repo.return_value = {
+        "name": "task-manager",
+        "language": "Python"
+}
+
+    service = RepositoryService(github_client)
 
     result = service.get_repository(
         "VishalNayek",
@@ -21,3 +25,24 @@ def test_get_repository_success():
         "name": "task-manager",
         "language": "Python"
     }
+
+def test_get_repository_not_found():
+
+    github_client = Mock()
+
+    error = requests.exceptions.HTTPError()
+
+    response = Mock()
+    response.status_code = 404
+
+    error.response = response
+
+    github_client.get_repo.side_effect = error
+
+    service = RepositoryService(github_client)
+
+    with pytest.raises(RepositoryNotFoundError):
+        service.get_repository(
+            "VishalNayek",
+            "does-not-exist"
+        )
